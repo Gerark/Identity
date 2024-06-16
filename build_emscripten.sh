eabfd410866f86d02312d62a940d182e5a18d5be
Gerark <gerrydm87@gmail.com>
2024-06-17 01:08:51 +0200

    Disable custom shells

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,3 +1,3 @@
-emcmake cmake -B build -DCMAKE_CXX_FLAGS="--preload-file ../data"
+emcmake cmake -B build -DCMAKE_CXX_FLAGS="--shell-file ../src/emscripten_shell.html --preload-file ../data"
 cd build
 emmake make
