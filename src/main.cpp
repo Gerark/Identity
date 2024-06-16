823867c7c1200d86e1aba0c5d2a0e5c08d852072
Gerark <st4lk3r87@gmail.com>
2024-06-16 03:38:39 +0200

    First cleanup attempt

--- src/main.cpp
+++ src/main.cpp
@@ -17,224 +17,178 @@
 #include "imgui/backends/imgui_impl_sdl2.h"
 #include "imgui/backends/imgui_impl_opengl3.h"
 #include <iostream>
+#include <functional>
 
-SDL_Window* g_window;
-SDL_GLContext context;
-ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-bool show_demo_window = true;
-bool show_another_window = false;
-int g_width;
-int g_height;
-
-// Function used by c++ to get the size of the html canvas
-/*EM_JS(int, canvas_get_width, (), {
-	return Module.canvas.width;
-	});
-	*/
-	// Function used by c++ to get the size of the html canvas
-	/*EM_JS(int, canvas_get_height, (), {
-		return Module.canvas.height;
-		});
-		*/
-		// Function called by javascript
-		/*EM_JS(void, resizeCanvas, (), {
-			js_resizeCanvas();
-		});*/
-int canvas_get_width() {
-	auto width = 0.0;
-	auto height = 0.0;
-	emscripten_get_element_css_size("#canvas", &width, &height);
-	return width;
-}
-
-int canvas_get_height() {
-	auto width = 0.0;
-	auto height = 0.0;
-	emscripten_get_element_css_size("#canvas", &width, &height);
-	return height;
-}
-
-void on_size_changed() {
-	// glfwSetWindowSize(g_window, g_width, g_height);
-
-	ImGui::SetCurrentContext(ImGui::GetCurrentContext());
-}
-
-void loop() {
-	int width = canvas_get_width();
-	int height = canvas_get_height();
-
-	if (width != g_width || height != g_height) {
-		g_width = width;
-		g_height = height;
-		on_size_changed();
-	}
-
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		ImGui_ImplSDL2_ProcessEvent(&event);
-	}
-
-	// glfwPollEvents();
-
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplSDL2_NewFrame();
-	ImGui::NewFrame();
-
-	// 1. Show a simple window.
-	// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-	{
-		static float f = 0.0f;
-		static int counter = 0;
-		ImGui::Text("Let's test just this!!!");                            // Display some text (you can use a format string too)
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-		ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our windows open/close state
-		ImGui::Checkbox("Another Window", &show_another_window);
-
-		if (ImGui::Button("Button")) // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-			counter++;
-		ImGui::SameLine();
-		ImGui::Text("counter = %d", counter);
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-	}
-
-	// 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-	if (show_another_window) {
-		ImGui::Begin("Another Window", &show_another_window);
-		ImGui::Text("Hello from another window!");
-		if (ImGui::Button("Close Me"))
-			show_another_window = false;
-		ImGui::End();
-	}
-
-	// 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-	if (show_demo_window) {
-		// ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-		// ImGui::ShowDemoWindow(&show_demo_window);
-	}
-
-	ImGui::Render();
-
-	//int display_w, display_h;
-	SDL_GL_MakeCurrent(g_window, context);
-	// glfwMakeContextCurrent(g_window);
-	// glfwGetFramebufferSize(g_window, &display_w, &display_h);
-	// glViewport(0, 0, 1000, 1000);
-	glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	auto error = glGetError();
-	if (error != GL_NO_ERROR) {
-		std::cout << "Error: " << error << std::endl;
-	}
-
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	// glfwMakeContextCurrent(g_window);
-	SDL_GL_SwapWindow(g_window);
-}
-
-int init_gl() {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
-		printf("Error: %s\n", SDL_GetError());
-		return -1;
-	}
-
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
-
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 #ifdef __EMSCRIPTEN__
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#else
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#endif
-	SDL_GL_SetSwapInterval(1);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-	// Open a window and create its OpenGL context
-	int canvasWidth = g_width;
-	int canvasHeight = g_height;
-	g_window =
-		SDL_CreateWindow("Antonino Liconti - CV",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			canvasWidth, canvasHeight,
+class EngineImpl {
+public:
+	void setupGLAttributes() {
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	}
+
+	SDL_Window* createWindow() {
+		emscripten_get_element_css_size("#canvas", &width, &height);
+		return SDL_CreateWindow("Antonino Liconti - CV",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			width,
+			height,
 			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
-
-	context = SDL_GL_CreateContext(g_window);
-	if (g_window == NULL) {
-		fprintf(stderr, "Failed to open GLFW window.\n");
-		//   glfwTerminate();
-		return -1;
 	}
-	SDL_GL_MakeCurrent(g_window, context);
-	// glfwMakeContextCurrent(g_window); // Initialize GLEW
 
-#ifndef __EMSCRIPTEN__
-	int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
-	if (version == 0) {
-		// print errors
+	SDL_GLContext createContext(SDL_Window* window) {
+		auto context = SDL_GL_CreateContext(window);
+		SDL_GL_MakeCurrent(window, context);
+		SDL_GL_SetSwapInterval(1);
+		return context;
 	}
-#endif
 
-	return 0;
-}
+	void run(const std::function<void()>& updateCallback) {
+		emscripten_set_main_loop(updateCallback, 0, 1);
+	}
+};
 
-int init_imgui() {
-	// Setup Dear ImGui binding
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplSDL2_InitForOpenGL(g_window, context);
-	ImGui_ImplOpenGL3_Init();
-
-	// Setup style
-	ImGui::StyleColorsDark();
-
-	ImGuiIO& io = ImGui::GetIO();
-
-	// Load Fonts
-	// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 23.0f);
-	// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 18.0f);
-	// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 26.0f);
-	// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 32.0f);
-	// io.Fonts->AddFontDefault();
-
-	// resizeCanvas();
-
-	return 0;
-}
-
-int init() {
-	init_gl();
-	init_imgui();
-	return 0;
-}
-
-void quit() {
-	// glfwTerminate();
-}
-
-extern "C" int main(int argc, char** argv) {
-	g_width = canvas_get_width();
-	g_height = canvas_get_height();
-	if (init() != 0)
-		return 1;
-
-#ifdef __EMSCRIPTEN__
-	emscripten_set_main_loop(loop, 0, 1);
 #else
-	while (true) {
-		loop();
+
+class EngineImpl {
+public:
+	void setupGLAttributes() {
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 	}
+
+	SDL_Window* createWindow() {
+		return SDL_CreateWindow("Antonino Liconti - CV",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			1280,
+			720,
+			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
+	}
+
+	SDL_GLContext createContext(SDL_Window* window) {
+		auto context = SDL_GL_CreateContext(window);
+		SDL_GL_MakeCurrent(window, context);
+		SDL_GL_SetSwapInterval(1);
+		int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+		if (version == 0) {
+			// print errors
+		}
+		return context;
+	}
+
+	void run(const std::function<void()>& updateCallback) {
+		while (true) {
+			updateCallback();
+		}
+	}
+};
+
 #endif
 
-	quit();
+class LittleEngine {
+public:
+	void init() {
+		_initSDL();
+		_engine.setupGLAttributes();
+		_window = _engine.createWindow();
+		_context = _engine.createContext(_window);
+		_initImGui();
+	}
+
+	void run(const std::function<void()>& updateCallback) {
+		_updateCallback = updateCallback;
+		_engine.run([this] () {
+			_internalLoop();
+		});
+	}
+
+private:
+	void _initSDL() {
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+			printf("Error: %s\n", SDL_GetError());
+		}
+		printf("TEST\n");
+	}
+
+	void _initImGui() {
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGui_ImplSDL2_InitForOpenGL(_window, _context);
+		ImGui_ImplOpenGL3_Init();
+
+		ImGui::StyleColorsDark();
+
+		ImGuiIO& io = ImGui::GetIO();
+
+		// Load Fonts
+		// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 23.0f);
+		// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 18.0f);
+		// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 26.0f);
+		// io.Fonts->AddFontFromFileTTF("data/xkcd-script.ttf", 32.0f);
+		// io.Fonts->AddFontDefault();
+	}
+
+	void _internalLoop() {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			ImGui_ImplSDL2_ProcessEvent(&event);
+			if (event.type == SDL_QUIT) {
+				SDL_Quit();
+				exit(0);
+			}
+		}
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL2_NewFrame();
+		ImGui::NewFrame();
+
+		_updateCallback();
+
+		ImGui::Render();
+
+		glClearColor(_clearColor.x, _clearColor.y, _clearColor.z, _clearColor.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		auto error = glGetError();
+		if (error != GL_NO_ERROR) {
+			std::cout << "Error: " << error << std::endl;
+		}
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		SDL_GL_SwapWindow(_window);
+	}
+
+	EngineImpl _engine;
+	SDL_Window* _window;
+	SDL_GLContext _context;
+	std::function<void()> _updateCallback;
+	ImVec4 _clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+};
+
+bool showWindow = true;
+extern "C" int main(int argc, char** argv) {
+	LittleEngine engine;
+	engine.init();
+
+	engine.run([] () {
+		ImGui::Begin("Tao Mei!", &showWindow);
+		ImGui::Text("Test Test Test!");
+		if (ImGui::Button("Close Me")) {
+			showWindow = false;
+		}
+		ImGui::End();
+	});
 
 	return 0;
 }
