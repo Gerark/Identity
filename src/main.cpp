3a93d1a7c13c7c1024c9531eae41b2b2d6c44aeb
Gerark <gerrydm87@gmail.com>
2024-06-16 23:56:05 +0200

    First example of loading an image

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,35 @@
 
 #include "imgui/imgui.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 bool showWindow = true;
 extern "C" int main(int /*argc*/, char** /*argv*/) {
 	LittleEngine engine;
 	engine.init();
 
-	engine.run([] () {
+	GLuint texture;
+
+	int width;
+	int height;
+	int channelsCount;
+	unsigned char* textureData = stbi_load("data/char.png", &width, &height, &channelsCount, 0);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, channelsCount > 3 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, textureData);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	stbi_image_free(textureData);
+
+	engine.run([texture] () {
 		ImGui::BeginMainMenuBar();
 
 		if (ImGui::BeginMenu("File")) {
@@ -22,6 +45,10 @@ extern "C" int main(int /*argc*/, char** /*argv*/) {
 			}
 			ImGui::EndMenu();
 		}
+
+		ImGui::Begin("Hello!");
+		ImGui::Image((void*)(intptr_t)texture, ImVec2(64, 64));
+		ImGui::End();
 
 		ImGui::EndMainMenuBar();
 		return true;
