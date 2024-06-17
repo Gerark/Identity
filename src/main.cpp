36b58d6242f5280b5cfe0b7071487aa86f95cee4
Gerark <st4lk3r87@gmail.com>
2024-06-17 12:19:49 +0200

    First stub for an app class

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,16 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
-bool showWindow = true;
-bool test = false;
-extern "C" int main(int /*argc*/, char** /*argv*/) {
-	LittleEngine engine;
-	engine.init();
+#include <unordered_map>
+#include <memory>
 
-	GLuint texture;
+struct Page {
+	std::function<void()> renderFunction;
+};
 
-	int width;
-	int height;
-	int channelsCount;
-	unsigned char* textureData = stbi_load("data/char.png", &width, &height, &channelsCount, 0);
-
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, channelsCount > 3 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, textureData);
-
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	stbi_image_free(textureData);
-
-	engine.run([texture] () {
+class App {
+public:
+	void render(GLuint texture) {
 		auto& style = ImGui::GetStyle();
 
 		ImGui::BeginMainMenuBar();
@@ -57,7 +40,40 @@ extern "C" int main(int /*argc*/, char** /*argv*/) {
 		ImGui::Begin("Hello!", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings);
 		ImGui::Image((void*)(intptr_t)texture, ImVec2(64, 64));
 		ImGui::End();
+	}
 
+private:
+	bool test = false;
+	std::unordered_map<std::string, std::unique_ptr<Page>> _pages;
+};
+
+extern "C" int main(int /*argc*/, char** /*argv*/) {
+	LittleEngine engine;
+	engine.init();
+
+	GLuint texture;
+
+	int width;
+	int height;
+	int channelsCount;
+	unsigned char* textureData = stbi_load("data/char.png", &width, &height, &channelsCount, 0);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, channelsCount > 3 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, textureData);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	stbi_image_free(textureData);
+
+	App app;
+	engine.run([texture, &app] () {
+		app.render(texture);
 		return true;
 	});
 
